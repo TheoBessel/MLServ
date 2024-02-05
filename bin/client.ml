9b223c28164c88bcb3522f543e1b275be91ba25d
Theo Bessel <theo.bessel@outlook.com>
2024-02-05 09:15:28 +0100

    [Resolves] : Code formatting

--- bin/client.ml
+++ bin/client.ml
@@ -1,6 +1,6 @@
 (* A web client written from scratch in OCaml *)
 
-let verbose : bool = false;;
+let verbose : bool = false
 
 let addr : Unix.sockaddr = Unix.ADDR_INET(Unix.inet_addr_of_string "127.0.0.1", 8000)
 
@@ -8,18 +8,18 @@ let dom : Unix.socket_domain = Unix.domain_of_sockaddr addr
 
 let typ : Unix.socket_type = Unix.SOCK_STREAM
 
-let flags : Unix.msg_flag list = [Unix.MSG_DONTROUTE];;
+let flags : Unix.msg_flag list = [Unix.MSG_DONTROUTE]
 
 
 
 let print_line_info(s : string) =
 	if verbose then
 		print_string s
-	else ();;
+	else ()
 
 
 
-let () =
+let main () : unit =
 	print_line_info "[1/4] Creating web socket ...";
 	let client : Unix.file_descr = Unix.socket dom typ 0 in
 	print_line_info "[1/4] Web socket sucessfully created !";
@@ -40,4 +40,6 @@ let () =
 
 		maintain_connexion := not ((Bytes.to_string packet) = "end connexion");
 	done;
-	print_line_info "[4/4] Connexion ended !";
+	print_line_info "[4/4] Connexion ended !"
+
+let _ = main ()
--- bin/server.ml
+++ bin/server.ml
@@ -1,48 +1,46 @@
 (* A web server written from scratch in OCaml *)
 
-let verbose : bool = false;;
+let verbose : bool = false
 
-let addr : Unix.sockaddr = Unix.ADDR_INET(Unix.inet_addr_of_string "127.0.0.1", 8000);;
+let addr : Unix.sockaddr = Unix.ADDR_INET(Unix.inet_addr_of_string "127.0.0.1", 8000)
 
-let dom : Unix.socket_domain = Unix.domain_of_sockaddr addr;;
+let dom : Unix.socket_domain = Unix.domain_of_sockaddr addr
 
-let typ : Unix.socket_type = Unix.SOCK_STREAM;;
+let typ : Unix.socket_type = Unix.SOCK_STREAM
 
-(*let packet : bytes = Bytes.of_string "Server response";;*)
+let flags : Unix.msg_flag list = [Unix.MSG_DONTROUTE]
 
-let flags : Unix.msg_flag list = [Unix.MSG_DONTROUTE];;
-
-let packet_size : int = 182;;
+let packet_size : int = 182
 
 
 let print_info(s : string) =
 	if verbose then
 		print_string s
-	else ();;
+	else ()
 
 let print_int_info(i : int) =
 	if verbose then
 		print_int i
-	else ();;
+	else ()
 
 let print_line_info(s : string) =
 	if verbose then
 		print_endline s
-	else ();;
+	else ()
 
 let print_addr (a : Unix.sockaddr) =
 	match a with
 		| Unix.ADDR_UNIX s -> print_string s
 		| Unix.ADDR_INET (i,p) -> let s = Unix.string_of_inet_addr i in
 				if s = "127.0.0.1" then print_string "localhost" else print_string s;
-				print_string "."; print_int p;;
+				print_string "."; print_int p
 
 let print_thread () : unit =
 	if verbose then
 		begin
 			print_string "[t"; print_int ((Domain.self ()) :> int); print_string "] - ";
 		end
-	else ();;
+	else ()
 
 
 
@@ -62,7 +60,7 @@ let rec handle_messages (socket : Unix.file_descr) (addr : Unix.sockaddr) =
 	print_thread (); print_line_info "------------------------------";
 	match (String.sub (Bytes.to_string buffer) 0 13) with
 		| "end connexion" -> ()
-		| _ -> handle_messages socket addr;;
+		| _ -> handle_messages socket addr
 
 
 
@@ -73,7 +71,7 @@ let connect_client (socket : Unix.file_descr) (addr : Unix.sockaddr) : unit =
 
 	handle_messages socket addr;
 
-	Unix.shutdown socket Unix.SHUTDOWN_RECEIVE;;
+	Unix.shutdown socket Unix.SHUTDOWN_RECEIVE
 
 
 
@@ -84,7 +82,7 @@ let rec handle_clients (server : Unix.file_descr) =
 	
 	print_line_info "[4/5] Creating a new thread ...";
 	let _ = Domain.spawn (fun () -> connect_client s a) in
-	handle_clients server;;
+	handle_clients server
 
 
 
@@ -99,6 +97,6 @@ let main () : unit =
 	print_line_info "[2/5] Sucessfully bound socket to address !";
 	
 	let t :  unit Domain.t = Domain.spawn (fun () -> handle_clients server) in
-	Domain.join t;;
+	Domain.join t
 	
-let _ = main ();
+let _ = main ()
